d3ddb4d638ac8dfcf4b0a3ab62ae37e3c0889168
marxteixeira <marxtts@gmail.com>
2025-07-28 19:13:43 -0300

    ajuste no sidebar desktop para exibir ícones quando tiver colapsada.

--- src/app/(panel)/dashboard/_components/sidebar.tsx
+++ src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -38,10 +38,6 @@ export function SidebarDashboard({ children }: { children: React.ReactNode}) {
                         alt="Logo da odontopro"
                         priority
                         quality={100}
-                        style={{
-                            width: 'auto',
-                            height: 'auto'
-                        }}
 
                     />
                     )}
@@ -53,6 +49,40 @@ export function SidebarDashboard({ children }: { children: React.ReactNode}) {
                 >
                     {!isCollapsed ? <ChevronLeft className='w-12 h-12' /> : <ChevronRight className='w-12 h-12' />}
                 </Button>
+
+                {/*Mostrar apenas quando a sidebar está recolhida*/}
+                {isCollapsed && (
+                    <nav className='flex flex-col gap-1 overflow-hidden mt-2'>
+                        <SidebarLink 
+                                    href="/dashboard"
+                                    label="Agendamentos"
+                                    pathname={pathname}
+                                    isCollapsed={isCollapsed}
+                                    icon={<CalendarCheck2 className='w-6 h-6' />}
+                            />
+                        <SidebarLink 
+                                    href="/dashboard/services"
+                                    label="Serviços"
+                                    pathname={pathname}
+                                    isCollapsed={isCollapsed}
+                                    icon={<Folder className='w-6 h-6' />}
+                        />
+                        <SidebarLink 
+                                    href="/dashboard/profile"
+                                    label="Meu perfil"
+                                    pathname={pathname}
+                                    isCollapsed={isCollapsed}
+                                    icon={<Settings className='w-6 h-6' />}
+                        />
+                        <SidebarLink 
+                                    href="/dashboard/plans"
+                                    label="Planos"
+                                    pathname={pathname}
+                                    isCollapsed={isCollapsed}
+                                    icon={<Folder className='w-6 h-6' />}
+                        />
+                    </nav>
+                )}
 
                 <Collapsible open={!isCollapsed}>
                     <CollapsibleContent>
