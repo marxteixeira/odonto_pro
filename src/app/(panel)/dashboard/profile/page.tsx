d1b0886e650d44111d6b3491dd9d49a3a2aa70ab
marxteixeira <marxtts@gmail.com>
2025-08-01 17:21:55 -0300

    criando uma camada de acesso a dados: DAL - Data Access Layer

--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,5 +1,21 @@
+import getSession from '@/lib/getSession'
+import { redirect } from 'next/navigation'
+import { getUserData } from "./_data-access/get-info-user";
 
-export default function Profile() {
+export default async function Profile() {
+    const session = await getSession()
+
+    if (!session) {
+        redirect("/")
+    }
+
+    const user = await getUserData({ userId: session.user?.id})
+    console.log("getUserData: ", user)
+
+    if (!user) {
+        redirect("/")
+    }
+
     return (
         <section>
             <h1>Página Perfil</h1>
