c6510be130745e13de229284b3ee385bfe9a8ddc
marxteixeira <marxtts@gmail.com>
2025-08-01 08:28:47 -0300

    chamando método de login no botão do Portal. Autenticancdo e redirecionando com o Github.

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -13,6 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { LogIn, Menu } from "lucide-react";
 import { useSession } from 'next-auth/react';
+import { handleRegister } from '../_actions/login';
 
 export function Header() {
     const { data: session, status } = useSession();
@@ -24,6 +25,10 @@ export function Header() {
     const navItems = [
         { href: "#profissionais", label: "Profissionais" }
     ]
+
+    async function handleLogin(){
+        await handleRegister("github")
+    }
 
     const NavLinks = () => (
         <>
@@ -46,12 +51,12 @@ export function Header() {
             ) : session ? (
                 <Link
                     href="/dashboard"
-                    className='flex items-center justify-center gap-2'
+                    className='flex items-center justify-center gap-2 bg-zinc-900  text-white py-1 px-4'
                 >
                     Acessar Clínica
                 </Link>
             ) : (
-                <Button>
+                <Button onClick={handleLogin}>
                     <LogIn />
                     Portal da Clínica
                 </Button>
