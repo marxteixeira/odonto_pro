ff7710b9e029e96236330d9c36b6a96e56e974eb
marxteixeira <marxtts@gmail.com>
2025-08-28 08:59:56 -0300

    dialog para formulário de serviço

--- src/app/(panel)/dashboard/services/_components/dialog-service.tsx
+++ src/app/(panel)/dashboard/services/_components/dialog-service.tsx
@@ -1,9 +1,23 @@
 "use client"
 
 import { DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
+import { useDialogServiceForm } from "./dialog-service-form"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
-export function DialogService(){
-    return(
+export function DialogService() {
+
+    const form = useDialogServiceForm()
+
+    return (
         <>
             <DialogHeader>
                 <DialogTitle> Novo Serviço</DialogTitle>
@@ -12,11 +26,104 @@ export function DialogService(){
                 </DialogDescription>
             </DialogHeader>
 
-            <div>
-                <h1>
-                    CONTEUDO DO MODAL
-                </h1>
-            </div>
+            <Form {...form}>
+                <form
+                    className="space-y-2">
+
+                    <div className="flex flex-col">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">
+                                        Nome do serviço:
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Digite o nome do serviço..."
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="price"
+                            render={({ field }) => (
+                                <FormItem className="my-2">
+                                    <FormLabel className="font-semibold">
+                                        Valor do serviço:
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Ex: 120,00"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <p className="font-semibold">Tempo de duração do serviço:</p>
+                        <div className="grid grid-cols-2 gap-3">
+                            <FormField
+                                control={form.control}
+                                name="hours"
+                                render={({ field }) => (
+                                    <FormItem className="my-2">
+                                        <FormLabel className="font-semibold">
+                                            Horas:
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="1"
+                                                min="0"
+                                                type="number"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="minutes"
+                                render={({ field }) => (
+                                    <FormItem className="my-2">
+                                        <FormLabel className="font-semibold">
+                                            Minutos:
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="0"
+                                                min="0"
+                                                type="number"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <Button
+                            type="submit"
+                            className="w-full font-semibold text-white"
+                        >
+                            Adicionar serviço
+                        </Button>
+
+                    </div>
+                </form>
+            </Form>
         </>
     )
 }
