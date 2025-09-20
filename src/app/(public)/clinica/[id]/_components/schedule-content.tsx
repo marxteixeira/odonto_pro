2a47214816b18331567c9c64329f638ba30c69d2
marxteixeira <marxtts@gmail.com>
2025-09-20 08:50:41 -0300

    criando campo com lista de Serviços

--- src/app/(public)/clinica/[id]/_components/schedule-content.tsx
+++ src/app/(public)/clinica/[id]/_components/schedule-content.tsx
@@ -11,6 +11,7 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { formatPhone } from '@/utils/formatPhone'
 import { DateTimePicker } from "./date-picker"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 type UserWithServiceAndSubscription = Prisma.UserGetPayload<{
     include: {
@@ -136,7 +137,7 @@ export function ScheduleContent({ clinic }: ScheduleContentProps) {
                                             onChange={(date) => {
                                                 if (date) {
                                                     field.onChange(date)
-                                                    
+
                                                 }
                                             }}
                                         />
@@ -145,6 +146,32 @@ export function ScheduleContent({ clinic }: ScheduleContentProps) {
                                 </FormItem>
                             )}
                         />
+
+                        <FormField
+                            control={form.control}
+                            name="serviceId"
+                            render={({ field }) => (
+                                <FormItem className="">
+                                    <FormLabel className="font-semibold">Selecione o serviço:</FormLabel>
+                                    <FormControl>
+                                        <Select onValueChange={field.onChange}>
+                                            <SelectTrigger className="w-full" >
+                                                <SelectValue placeholder="Selecione um serviço" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                {clinic.services.map((service) => (
+                                                    <SelectItem key={service.id} value={service.id}>
+                                                        {service.name} - {Math.floor(service.duration / 60)}h {service.duration % 60}min
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
 
                     </form>
                 </Form>
