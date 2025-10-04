0aedab29291c8592f7cb3eb9d600c4eaff9978e9
marxteixeira <marxtts@gmail.com>
2025-10-04 09:14:24 -0300

    Criando componente para agenda. data-access para buscar horários da clínica. Hook urlState para pegar parâmetros pela rota.

---
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useSearchParams } from 'next/navigation'
+
+interface AppointmentsListProps {
+    times: string[]
+}
+
+export function AppointmentsList({ times }: AppointmentsListProps) {
+
+    const searchParams = useSearchParams();
+    const date = searchParams.get("date")
+
+    console.log(date);
+
+    return (
+        <div>
+            LISTAGEM DE HORIARIOS
+        </div>
+    )
+}
