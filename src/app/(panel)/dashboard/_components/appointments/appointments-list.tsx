ee1602d347044d5aa893c7496ff317dd2679ed67
marxteixeira <marxtts@gmail.com>
2025-10-07 08:23:20 -0300

    contruindo a grid de horários em uma scrollArea

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -1,6 +1,14 @@
 "use client"
 
 import { useSearchParams } from 'next/navigation'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+    CardDescription
+} from '@/components/ui/card'
 
 interface AppointmentsListProps {
     times: string[]
@@ -14,8 +22,32 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
     console.log(date);
 
     return (
-        <div>
-            LISTAGEM DE HORIARIOS
-        </div>
+        <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                <CardTitle className='text-xl md:text-2xl font-bold'>
+                    Agendamentos
+                </CardTitle>
+                <button>SELECIONAR DATA</button>
+            </CardHeader>
+
+            <CardContent>
+                <ScrollArea className='h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4'>
+                    {times.map((slot) => {
+
+                        return (
+                            <div
+                                key={slot}
+                                className='flex items-center py-2 border-t last:border-b'
+                            >
+                                <div className='w-16 text-sm font-semibold'>{slot}</div>
+                                <div className='flex-1 text-sm text-gray-500'>
+                                    Disponível
+                                </div>
+                            </div>
+                        )
+                    })}
+                </ScrollArea>
+            </CardContent>
+        </Card>
     )
 }
