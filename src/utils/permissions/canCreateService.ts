c963e7c85eb0d25867678e020e5624975a2856ed
marxteixeira <marxtts@gmail.com>
2025-10-21 22:54:20 -0300

    funçoes para limitar de acordo com a assinatura ou com o período trial.

---
+++ src/utils/permissions/canCreateService.ts
@@ -0,0 +1,45 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { ResultPermissionProp } from "./canPermission";
+import { Session } from "next-auth";
+import { getPlan } from "./get-plans";
+import { PLANS } from "../plans";
+import { Subscription } from "@/prisma-client";
+import { checkSubscriptionExpired } from "./checkSubscriptionExpired";
+
+export async function canCreateService(subscription: Subscription | null, session: Session): Promise<ResultPermissionProp> {
+    try {
+        const serviceCount = await prisma.service.count({
+            where: {
+                userId: session?.user?.id
+            }
+        })
+
+        if (subscription && subscription.status === "active") {
+            const plan = subscription.plan;
+            const planLimits = await getPlan(plan);
+
+            console.log("LIMITES DO SEU PLANO: ", planLimits)
+
+            return {
+                hasPermission: planLimits.maxServices === null || serviceCount <= planLimits.maxServices,
+                planId: subscription.plan,
+                expired: false,
+                plan: PLANS[subscription.plan],
+            }
+        }
+
+        const checkUserLimit = await checkSubscriptionExpired(session)
+
+        return checkUserLimit;
+
+    } catch (err) {
+        return {
+            hasPermission: false,
+            planId: "EXPIRED",
+            expired: false,
+            plan: null,
+        }
+    }
+}
